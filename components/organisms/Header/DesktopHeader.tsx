09d9a43b3d1ffb6385a75849dd282009503de280
Ranu <ranushka543@gmail.com>
2022-02-14 07:36:11 +0400

    search advance refinment

--- components/organisms/Header/DesktopHeader.tsx
+++ components/organisms/Header/DesktopHeader.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import classnames from 'classnames'
 import Skeleton from 'react-loading-skeleton'
 import styles from './desktop.module.scss'
-import { ActionItem, Space, Logo } from 'components/atoms'
-import { SearchForm, CategoriesMenu } from 'components/molecules'
-import { IconArrowDown } from 'components/icons'
+import { ActionItem, Space, Logo, ButtonFab } from 'components/atoms'
+import {
+  SearchForm,
+  SearchFormAdvanced,
+  CategoriesMenu,
+} from 'components/molecules'
+import { IconArrowDown, IconClear } from 'components/icons'
 import { attributes as t } from 'data/header.md'
 import { useSession } from 'next-auth/react'
+import useGlobel from 'context/GlobelContext'
 
 const DesktopHeader: React.FC = ({}) => {
   const { data: session, status } = useSession()
   const btnGuestOrUser = session ? __getLoggedInUser(session) : __getLoginBtn()
+  const [state, setState] = useGlobel()
   const loading = status === 'loading'
 
   return (
@@ -28,13 +34,31 @@ const DesktopHeader: React.FC = ({}) => {
       </header>
       <div
         key="searchContainer"
-        className={classnames('bg__white stage__2 ', styles.searchContainer)}
+        className={classnames(
+          'bg__white stage__2',
+          !state.toggleAdvanceSearch && 'sticky',
+          styles.searchContainer
+        )}
       >
         <section className="flex__center mw__7 px__3 py__3">
           <Logo flag className={'flex__center mw__1'} />
           <CategoriesMenu />
           <div className="flex flex__center mw__4 block">
-            <SearchForm id="mainSearch" />
+            {state.toggleAdvanceSearch ? (
+              <div className="flex__center">
+                <h2 className="color__nut7">Advanced Search</h2>
+                <ButtonFab
+                  href="/search"
+                  icon={<IconClear />}
+                  classNames={'pointer rounded'}
+                  onClick={() =>
+                    setState({ ...state, toggleAdvanceSearch: false })
+                  }
+                />
+              </div>
+            ) : (
+              <SearchForm id="mainSearch" />
+            )}
           </div>
 
           <ActionItem
@@ -47,6 +71,15 @@ const DesktopHeader: React.FC = ({}) => {
           </div>
         </section>
       </div>
+      {state.toggleAdvanceSearch && (
+        <div key="advancedSearch" className={classnames('bg__white stage__2 ')}>
+          <section className="flex__center mw__7 px__3 py__3">
+            <div className="flex flex__center block">
+              <SearchFormAdvanced id="advancedSearch" />
+            </div>
+          </section>
+        </div>
+      )}
     </>
   )
 }
