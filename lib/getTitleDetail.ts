ea2e46dcb6578c809daea4293f598ae6aa2a2cc4
Ranu <ranushka543@gmail.com>
2022-03-05 20:00:26 +0400

    refinement

--- lib/getTitleDetail.ts
+++ lib/getTitleDetail.ts
@@ -1,39 +1,4 @@
 import fetch from 'lib/request'
-// import { tempPublicationUrl } from 'lib/helpers'
-
-// const __makeItRand = () => Math.random() >= 0.5;
-
-// const __processData = (data) => {
-//   let keywords = [];
-
-//   const results = data.results.map((item) => {
-//     const href = tempPublicationUrl(item.bibjson.title, item.id);
-//     let subjects = [];
-
-//     if (item.bibjson && item.bibjson.subject) {
-//       subjects = item.bibjson.subject.map((a) => a.term);
-//     }
-
-//     return {
-//       ...item.bibjson,
-//       href,
-//       id: item.id,
-//       subjects,
-//       isReviewed: __makeItRand(),
-//       referenced: __makeItRand(),
-//     };
-//   });
-
-//   if (results) {
-//     keywords = results
-//       .map((a) => a.keywords)
-//       .filter((a) => a)
-//       .flat()
-//       .slice(0, 12);
-//   }
-
-//   return { ...data, results, keywords };
-// };
 
 const getTitleDetail = async (article_id: string) => {
   const data = await fetch(`https://doaj.org/api/v1/articles/${article_id}`)
