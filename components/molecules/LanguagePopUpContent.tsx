896f3fc4593751e80296f0960951079a739f616e
ranu <ranushka543@gmail.com>
2022-06-19 01:04:56 +0400

    refinment

--- components/molecules/LanguagePopUpContent.tsx
+++ components/molecules/LanguagePopUpContent.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { useTranslations } from 'next-intl'
 import { SelectLanguage } from 'components/molecules'
 
 const LanguagePopUpContent = () => {
-  const trans = useTranslations('settings')
-
   return (
     <div className="px-8 py-8">
-      <h2 className="text-xl font-medium mb-4 font-serif">{trans('title')}</h2>
+      <h2 className="text-xl font-medium mb-4 font-serif">Select language</h2>
       <SelectLanguage />
     </div>
   )
