cc1d9261195a92aa82fb30cb8026417aabd77380
ranu <ranushka543@gmail.com>
2022-07-03 20:28:57 +0400

    refinment

--- utilApi/apiSolrSearchById.ts
+++ utilApi/apiSolrSearchById.ts
@@ -1,20 +1,27 @@
 import formatSearchResult from 'utilApi/formatSearchResult'
 
-const url = `http://3.67.163.226:8983/solr/openaccess/select?q=zendy_id:`
+const SOLR_TOKEN = process.env.SOLR_TOKEN
+const SOLR_URL = process.env.SOLR_URL
+const url = `${SOLR_URL}/query?q=zendy_id:`
 
 export default async function apiSolrSearchById(resultId) {
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'Basic cmFudTpjeXdFWWpqNkdacTB1OWV3'
+      Authorization: `Basic ${SOLR_TOKEN}`
     }
   }
 
-  const fullUrl = url + resultId
-  const res = await fetch(fullUrl, options)
-  const data = await res.json()
-  const formattedData = formatSearchResult(data.response.docs, null)
+  try {
+    const fullUrl = url + resultId
+    const res = await fetch(fullUrl, options)
+    const data = await res.json()
 
-  return formattedData
+    const formattedData = formatSearchResult(data.response.docs, null)
+
+    return formattedData
+  } catch (error) {
+    throw new Error(JSON.stringify(error))
+  }
 }
