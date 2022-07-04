8fe05fad3b90390411627ac0edb3c40358b67a55
ranu <ranushka543@gmail.com>
2022-07-04 07:10:42 +0400

    test

--- components/organisms/DetailPdf.tsx
+++ components/organisms/DetailPdf.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
+// import { Document, Page, pdfjs } from 'react-pdf'
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const DetailPdf: React.FC<any> = ({ pdfUrl }) => {
   // const [numPages, setNumPages] = React.useState(null)
 
+  // function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+  //   setNumPages(nextNumPages)
+  // }
+
+  // function onLoadError() {
+  //   fetch(pdfUrl, {
+  //     headers: {
+  //       Accept: 'application/octet-stream'
+  //     }
+  //   })
+  //     .then((res) => {
+  //       return res.arrayBuffer()
+  //     })
+  //     .then((data) => {
+  //       console.log('--------------')
+  //       console.log(data)
+  //     })
+  //     .catch((err) => {
+  //       return err.Message
+  //     })
+  // }
+
   return (
     <div>
-      <embed className="w-full min-h-screen" src={pdfUrl + '#toolbar=0'} />
+      {/* 
+      <embed className="w-full min-h-screen" src={pdfUrl + '#toolbar=0'} /> 
+      */}
+
+      {pdfUrl}
+
+      {/* <embed src={pdfUrl} type="application/pdf" height={800} width={500} /> */}
+
+      {/* <Document
+        file={pdfUrl}
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={onLoadError}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document> */}
     </div>
   )
 }
