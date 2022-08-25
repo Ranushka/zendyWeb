4a233774512c6e99f72b01c662ad630e9ba4e50f
ranu <ranushka543@gmail.com>
2022-08-26 03:08:23 +0400

    refinment

--- snapshots.js
+++ snapshots.js
@@ -1,24 +1,23 @@
 const PREVIEW_URL = process.env.VERCEL_URL
 
+console.log(process.env)
+
 module.exports = [
   {
     name: 'Home',
-    url: PREVIEW_URL + '',
-    waitForTimeout: '8000'
+    url: PREVIEW_URL + ''
   },
   {
     name: 'Platform',
-    url: PREVIEW_URL + '/platform',
-    waitForTimeout: '8000'
+    url: PREVIEW_URL + '/platform'
   },
   {
     name: 'Pricing',
-    url: PREVIEW_URL + '/pricing',
-    waitForTimeout: '8000'
+    url: PREVIEW_URL + '/pricing'
   },
   {
     name: 'Search query',
     url: PREVIEW_URL + '/search?q=artificial+intelligence',
-    waitForTimeout: '8000'
+    waitForTimeout: 8000
   }
 ]
