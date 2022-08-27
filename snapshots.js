43d6f20e61994d1355a7b8a77b4bce51f0f1ac7b
ranu <ranushka543@gmail.com>
2022-08-27 17:03:48 +0400

    test workflows

--- snapshots.js
+++ snapshots.js
@@ -5,21 +5,21 @@ console.log('url--', PREVIEW_URL)
 
 module.exports = [
   {
-    name: 'Home',
-    url: PREVIEW_URL,
-    waitForTimeout: 5000
-  },
-  {
-    name: 'Platform',
-    url: PREVIEW_URL + '/platform'
+    name: 'Search query',
+    url: PREVIEW_URL + '/search?q=artificial+intelligence',
+    waitForTimeout: 8000
   },
   {
     name: 'Pricing',
     url: PREVIEW_URL + '/pricing'
   },
   {
-    name: 'Search query',
-    url: PREVIEW_URL + '/search?q=artificial+intelligence',
-    waitForTimeout: 8000
+    name: 'Platform',
+    url: PREVIEW_URL + '/platform'
+  },
+  {
+    name: 'Home',
+    url: PREVIEW_URL,
+    waitForTimeout: 5000
   }
 ]
